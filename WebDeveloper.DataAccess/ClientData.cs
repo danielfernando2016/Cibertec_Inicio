256c9f42624cca08e5c2252b2e9ec0ace3cf1cd4
danielfernando2016 <danielfernando2010@gmail.com>
2016-06-15 21:44:45 -0500

    Actualizacion del Update y Delete del Modelo

--- WebDeveloper.DataAccess/ClientData.cs
+++ WebDeveloper.DataAccess/ClientData.cs
@@ -5,23 +5,31 @@ using WebDeveloper.Model;
 
 namespace WebDeveloper.DataAccess
 {
-    public class ClientData
+    public class ClientData: BaseDataAccess<Client>
     {
-        public List<Client> GetFakeData()
-        {
-            return new List<Client>
-            {
-                //new Client { ID=1,Name ="Juan",LastName="Perez" },
-                //new Client { ID=2,Name ="Raul",LastName="Ruidiaz" }
-            };
-        }
+
+        //public List<Client> GetFakeData()
+        //{
+        //    return new List<Client>
+        //    {
+        //        new Client { ID=1,Name ="Juan",LastName="Perez" },
+        //        new Client { ID=2,Name ="Raul",LastName="Ruidiaz" }
+        //    };
+        //}
 
 
-        public List<Client> GetList()
+        //public List<Client> GetList()
+        //{
+        //    using (var dbContext = new WebContextDb())
+        //    {
+        //        return dbContext.Clients.ToList();
+        //    }
+        //}
+        public Client GetClientById(int id)
         {
             using (var dbContext = new WebContextDb())
             {
-                return dbContext.Clients.ToList();
+                return dbContext.Clients.FirstOrDefault(x => x.ID == id);                
             }
         }
 
--- WebDeveloper/Controllers/ClientController.cs
+++ WebDeveloper/Controllers/ClientController.cs
@@ -4,16 +4,90 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebDeveloper.DataAccess;
+using WebDeveloper.Model;
 
 namespace WebDeveloper.Controllers
 {
     public class ClientController : Controller
     {
+        private ClientData _client = new ClientData();
         // GET: Client
         public ActionResult Index()
         {
-            var client = new ClientData();
-            return View(client.GetFakeData());
+            //var client = new ClientData();
+            //return View(client.GetFakeData());
+
+            //var client = new ClientData();
+            return View(_client.GetList());
+
         }
+
+        public ActionResult Create()
+        {
+            return View(new Client());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Client client)
+        {
+            //Primero
+            if (ModelState.IsValid)
+            {
+                _client.Add(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Edit
+        public ActionResult Edit(int id)
+        {
+
+            
+            var client = _client.GetClientById(id);            
+            if (client==null)
+                RedirectToAction("Index");
+            return View(client);
+
+        }
+
+        //Edit Post
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            //Primero
+            if (ModelState.IsValid)
+            {
+                _client.Update(client);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        //Delete
+        public ActionResult Delete(int id)
+        {
+
+
+            var client = _client.GetClientById(id);
+            if (client == null)
+                RedirectToAction("Index");
+            return View(client);
+
+        }
+
+        //Edit Post
+        [HttpPost]
+        public ActionResult Delete(Client client)
+        {
+            if (_client.Delete(client)>0)
+                return RedirectToAction("Index");
+            
+            return View();
+        }
+
+
+
     }
 }
