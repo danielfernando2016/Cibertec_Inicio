e7266ad57fd01f6e9265a4bd05c6fe7ab11f54c0
danielfernando2016 <danielfernando2010@gmail.com>
2016-06-20 22:31:30 -0500

    20/06/2016 Cambios 2: Se ha agregado una Lista del mi Clase CLient a mi Controlador Razor

---
+++ WebDeveloper/Controllers/RazorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebDeveloper.Model;
+
+namespace WebDeveloper.Controllers
+{
+    public class RazorController : Controller
+    {
+        // GET: Razor
+        public ActionResult Index()
+        {
+           
+            List<Client> list = new List<Client>()         
+            {
+                new Client { ID = 1, Name = "Jose", LastName = "Perez" },
+                new Client { ID = 2, Name = "Juan", LastName = "Lopez" }
+            }; 
+
+            return View(list);
+        }
+    }
+}
