1c74eb1dbb25aec61134f9772b4b6157c3a2f6cf
danielfernando2016 <danielfernando2010@gmail.com>
2016-06-27 22:24:45 -0500

    Clase 27/06/2016: Routes ActionFilter Logs

--- WebDeveloper/App_Start/RouteConfig.cs
+++ WebDeveloper/App_Start/RouteConfig.cs
@@ -13,11 +13,32 @@ namespace WebDeveloper
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Razor",
-                url: "Razor/{id}",
-                defaults: new { controller = "Razor", action = "Product", id = UrlParameter.Optional }
-            );
+            ////Razor
+            //routes.MapRoute(
+            //    name: "RazorId",
+            //    url: "Razor/{id}",
+            //    defaults: new
+            //    {
+            //        controller = "Razor",
+            //        action = "EntryId",
+
+            //    }
+            //);
+
+            ////Razor
+            //routes.MapRoute(
+            //    name: "RazorDate",
+            //    url: "Razor/{date}",
+            //    defaults: new
+            //    {
+            //        controller = "Razor",
+            //        action = "EntryDate",
+
+            //    }
+            //);
+
+
+            routes.MapMvcAttributeRoutes();
 
 
             routes.MapRoute(
